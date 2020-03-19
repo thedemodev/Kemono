c4631217d3eefe2212f8e748da319dc6c699a422
Anonymous <>
2020-03-18 21:26:01 -0400

    revert

--- public/js/user.js
+++ public/js/user.js
@@ -82,9 +82,9 @@ async function main() {
       </div>
     </div>
   `
-}
-document.getElementById('martha-view').innerHTML += `
+  marthaView.innerHTML += `
     <button onClick="loadMorePosts(26)" id="load-more-button" class="load-more-button">Load More</a>
-`
+  `
+  loadMorePosts(0)
+}
 main()
-loadMorePosts(0)
