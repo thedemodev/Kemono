58082b865ac5bc469d5200c522397a668dc43b9e
Anonymous <>
2020-03-18 20:32:52 -0400

    faster recent posts loading

--- public/js/index.js
+++ public/js/index.js
@@ -81,79 +81,78 @@ async function searchUpdate() {
 }
 
 async function main() {
+  let marthaView = document.getElementById('recent-view');
   const recentData = await fetch('/api/recent');
   const recent = await recentData.json();
   recent.map(async(post) => {
     if (post.version == 1 || post.service == 'patreon') {
-      const userData = await fetch(`/proxy/user/${post.user}`);
-      const user = await userData.json();
-
-      let marthaView = document.getElementById('recent-view');
       marthaView.innerHTML += `
         <div class="recent-row">
           <div class="recent-row-container">
-            <a href="/user/${user.data.id}">
-              <div class="avatar" style="background-image: url('${user.included[0].attributes.avatar_photo_url}');"></div>
-            </a>
+            <a href="/user/${post.user}" id="post-${post.id}-avatar"></a>
             <div style="display: inline-block">
-              <a class="link-reset" href="/user/${user.data.id}">
+              <a class="link-reset" href="/user/${post.user}">
                 <p><b>${post.title}</b></p>
               </a>
-              <a class="link-reset" href="/user/${user.data.id}">
-                <p>${user.data.attributes.vanity || user.data.attributes.full_name}</p>
-              </a>
+              <a class="link-reset" href="/user/${user.data.id}" id="post-${post.id}-username"></a>
             </div>
           </div>
         </div>
       `
+      fetch(`/proxy/user/${post.user}`)
+        .then(data => data.json())
+        .then(user => {
+          let avatar = document.getElementById(`post-${post.id}-avatar`);
+          avatar += `<div class="avatar" style="background-image: url('${user.included[0].attributes.avatar_photo_url}');"></div>`
+          let username = document.getElementById(`post-${post.id}-username`);
+          username += `<p>${user.data.attributes.vanity || user.data.attributes.full_name}</p>`
+        })
     } else if (post.service == 'gumroad') {
-      const userData = await fetch(`/proxy/gumroad/user/${post.user}`);
-      const user = await userData.json();
-
-      let marthaView = document.getElementById('recent-view');
       marthaView.innerHTML += `
         <div class="recent-row">
           <div class="recent-row-container">
-            <a href="/gumroad/user/${post.user}">
-              <div class="avatar" style="background-image: url('${user.avatar}');"></div>
-            </a>
+            <a href="/gumroad/user/${post.user}" id="post-${post.id}-avatar"></a>
             <div style="display: inline-block">
               <a class="link-reset" href="/gumroad/user/${post.user}">
                 <p><b>${post.title}</b></p>
               </a>
-              <a class="link-reset" href="/gumroad/user/${post.user}">
-                <p>${user.name}</p>
-              </a>
+              <a class="link-reset" href="/gumroad/user/${user.data.id}" id="post-${post.id}-username"></a>
             </div>
           </div>
         </div>
       `
+      fetch(`/proxy/gumroad/user/${post.user}`)
+        .then(data => data.json())
+        .then(user => {
+          let avatar = document.getElementById(`post-${post.id}-avatar`);
+          avatar += `<div class="avatar" style="background-image: url('${user.avatar}');"></div>`
+          let username = document.getElementById(`post-${post.id}-username`);
+          username += `<p>${user.name}</p>`
+        })
     } else if (post.service == 'fanbox') {
-      require(["https://unpkg.com/unraw@1.2.5/dist/index.min.js"], function(unraw) {
-        fetch(`/proxy/fanbox/user/${post.user}`)
-          .then(userData => userData.json())
-          .then(user => {
-            let marthaView = document.getElementById('recent-view');
-            marthaView.innerHTML += `
-              <div class="recent-row">
-                <div class="recent-row-container">
-                  <a href="/fanbox/user/${post.user}">
-                    <div class="avatar" style="background-image: url('${unraw.unraw(user.body.user.iconUrl)}');"></div>
-                  </a>
-                  <div style="display: inline-block">
-                    <a class="link-reset" href="/fanbox/user/${post.user}">
-                      <p><b>${post.title}</b></p>
-                    </a>
-                    <a class="link-reset" href="/fanbox/user/${post.user}">
-                      <p>${unraw.unraw(user.body.user.name)}</p>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            `
+      marthaView.innerHTML += `
+        <div class="recent-row">
+          <div class="recent-row-container">
+            <a href="/fanbox/user/${post.user}" id="post-${post.id}-avatar"></a>
+            <div style="display: inline-block">
+              <a class="link-reset" href="/fanbox/user/${post.user}">
+                <p><b>${post.title}</b></p>
+              </a>
+              <a class="link-reset" href="/fanbox/user/${user.data.id}" id="post-${post.id}-username"></a>
+            </div>
+          </div>
+        </div>
+      `
+      fetch(`/proxy/fanbox/user/${post.user}`)
+        .then(data => data.json())
+        .then(user => {
+          require(["https://unpkg.com/unraw@1.2.5/dist/index.min.js"], unraw => {
+            let avatar = document.getElementById(`post-${post.id}-avatar`);
+            avatar += `<div class="avatar" style="background-image: url('${unraw.unraw(user.body.user.iconUrl)}');"></div>`
+            let username = document.getElementById(`post-${post.id}-username`);
+            username += `<p>${unraw.unraw(user.body.user.name)}</p>`
           })
-          
-      })
+        })
     }
   });
   document.getElementById('search-input').addEventListener('keyup', _.debounce(searchUpdate, 350))
