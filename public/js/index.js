974fc7c8b4b3d294b1213cce5f2da8d098e42d10
Anonymous <>
2020-03-18 21:13:04 -0400

    Revert "Update index.js"
    This reverts commit 71f90a49593c5718b2df140efacb3734aed7a19b.

--- public/js/index.js
+++ public/js/index.js
@@ -86,7 +86,6 @@ async function main() {
   const recent = await recentData.json();
   recent.map(async(post) => {
     if (post.version == 1 || post.service == 'patreon') {
-      let marthaView = document.getElementById('recent-view');
       marthaView.innerHTML += `
         <div class="recent-row">
           <div class="recent-row-container">
@@ -109,7 +108,6 @@ async function main() {
           username += `<p>${user.data.attributes.vanity || user.data.attributes.full_name}</p>`
         })
     } else if (post.service == 'gumroad') {
-      let marthaView = document.getElementById('recent-view');
       marthaView.innerHTML += `
         <div class="recent-row">
           <div class="recent-row-container">
@@ -132,7 +130,6 @@ async function main() {
           username += `<p>${user.name}</p>`
         })
     } else if (post.service == 'fanbox') {
-      let marthaView = document.getElementById('recent-view');
       marthaView.innerHTML += `
         <div class="recent-row">
           <div class="recent-row-container">
@@ -161,4 +158,4 @@ async function main() {
   document.getElementById('search-input').addEventListener('keyup', _.debounce(searchUpdate, 350))
 }
 
-main()
+window.onload = main()
