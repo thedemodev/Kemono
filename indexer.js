1ddba0043a26947a6f887b8e7dd4af23a0fa7598
Anonymous <>
2020-03-19 18:42:27 -0400

    debug indexer

--- indexer.js
+++ indexer.js
@@ -8,62 +8,62 @@ async function indexer() {
   posts
     .find({})
     .sort({ added_at: -1 })
-    .forEach(async(post) => {
+    .forEach(post => {
       console.log('post hit!'); //debug
-      let indexExists = await lookup.findOne({id: post.user});
-      if (indexExists) return;
+      // let indexExists = await lookup.findOne({id: post.user});
+      // if (indexExists) return;
 
-      switch (post.service) {
-        case 'patreon': {
-          console.log('new patreon!'); //debug
-          let api = 'https://www.patreon.com/api/user';
-          let user = await cloudscraper.get(`${api}/${post.user}`, { json: true });
-          await lookup.insertOne({
-            version: post.version,
-            service: 'patreon',
-            id: post.user,
-            name: user.data.attributes.vanity || user.data.attributes.full_name
-          });
-          break;
-        }
-        case 'fanbox': {
-          let api = 'https://fanbox.pixiv.net/api/creator.get?userId';
-          let user = await request.get(`${api}=${post.user}`, { 
-            json: true,
-            headers: {
-              'origin': 'https://www.pixiv.net'
-            } 
-          });
-          await lookup.insertOne({
-            version: post.version,
-            service: 'fanbox',
-            id: post.user,
-            name: unraw(user.body.user.name)
-          });
-          break;
-        }
-        case 'gumroad': {
-          let api = 'https://kemono.party/proxy/gumroad/user';
-          let user = await request.get(`${api}/${post.user}`, { json: true });
-          await lookup.insertOne({
-            version: post.version,
-            service: 'gumroad',
-            id: post.user,
-            name: user.name
-          })
-          break;
-        }
-        default: {
-          let api = 'https://www.patreon.com/api/user';
-          let user = await cloudscraper.get(`${api}/${post.user}`, { json: true });
-          await lookup.insertOne({
-            version: post.version,
-            service: 'patreon',
-            id: post.user,
-            name: user.data.attributes.vanity || user.data.attributes.full_name
-          });
-        }
-      }
+      // switch (post.service) {
+      //   case 'patreon': {
+      //     console.log('new patreon!'); //debug
+      //     let api = 'https://www.patreon.com/api/user';
+      //     let user = await cloudscraper.get(`${api}/${post.user}`, { json: true });
+      //     await lookup.insertOne({
+      //       version: post.version,
+      //       service: 'patreon',
+      //       id: post.user,
+      //       name: user.data.attributes.vanity || user.data.attributes.full_name
+      //     });
+      //     break;
+      //   }
+      //   case 'fanbox': {
+      //     let api = 'https://fanbox.pixiv.net/api/creator.get?userId';
+      //     let user = await request.get(`${api}=${post.user}`, { 
+      //       json: true,
+      //       headers: {
+      //         'origin': 'https://www.pixiv.net'
+      //       } 
+      //     });
+      //     await lookup.insertOne({
+      //       version: post.version,
+      //       service: 'fanbox',
+      //       id: post.user,
+      //       name: unraw(user.body.user.name)
+      //     });
+      //     break;
+      //   }
+      //   case 'gumroad': {
+      //     let api = 'https://kemono.party/proxy/gumroad/user';
+      //     let user = await request.get(`${api}/${post.user}`, { json: true });
+      //     await lookup.insertOne({
+      //       version: post.version,
+      //       service: 'gumroad',
+      //       id: post.user,
+      //       name: user.name
+      //     })
+      //     break;
+      //   }
+      //   default: {
+      //     let api = 'https://www.patreon.com/api/user';
+      //     let user = await cloudscraper.get(`${api}/${post.user}`, { json: true });
+      //     await lookup.insertOne({
+      //       version: post.version,
+      //       service: 'patreon',
+      //       id: post.user,
+      //       name: user.data.attributes.vanity || user.data.attributes.full_name
+      //     });
+      //   }
+      // }
     }, () => {})
 }
 
