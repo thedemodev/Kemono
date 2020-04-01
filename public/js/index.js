e77c493dc51408fe1eac920d7775482b5f5d742c
Anonymous <>
2020-03-31 20:27:10 -0400

    debounce search

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,18 @@
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+}
+
 async function searchUpdate() {
   let marthaView = document.getElementById('recent-view');
   marthaView.innerHTML = ''
@@ -181,7 +196,7 @@ async function main() {
       })
     }
   });
-  document.getElementById('search-input').addEventListener('keyup', () => searchUpdate())
+  document.getElementById('search-input').addEventListener('keyup', debounce(() => searchUpdate(), 350))
 }
 
 main()
